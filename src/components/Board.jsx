6710be0c641a46d83227d39c71fdfdadfd201da7
FatimeIbrahimova <fatime.ibrahimova44@gmail.com>
2023-01-06 18:10:47 +0400

    delete added

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -4,13 +4,19 @@ import CheckIcon from '@mui/icons-material/Check';
 import { MainContext } from '../context/ContextProvider';
 
 const Board = () => {
-    const { todos } = useContext(MainContext)
+    const { todos, setTodos } = useContext(MainContext)
+
+    const deleteTodo = (index) => {
+        alert("index of todo:" + index)
+        const newList = todos;
+        newList.splice(index, 1)
+        setTodos([...newList])
+    }
     return (
         <div >
             {todos.map((t, index) => (
                 <div className='todo-row'>
-                    <li style={{ listStyle: "none", display: "flex", justifyContent: "space-around" }} key={index}>{t} <div><CloseRoundedIcon /> <CheckIcon /></div></li>
-
+                    <li style={{ listStyle: "none", display: "flex", justifyContent: "space-around" }} key={index}>{t} <div ><CloseRoundedIcon onClick={() => deleteTodo(index)} /> <CheckIcon /></div></li>
                 </div>
             ))}
         </div>
